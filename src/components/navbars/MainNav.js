891bad358e23aae56d2abd91a05d2ee9b92f098a
malvaniTourism <97733182+malvaniTourism@users.noreply.github.com>
2022-05-01 21:58:31 +0530

    Update MainNav.js

--- src/components/navbars/MainNav.js
+++ src/components/navbars/MainNav.js
@@ -1,25 +1,39 @@
 import React from "react";
 
 const MainNav = () => {
-
     return (
-        <nav class="navbar navbar-expand-lg sticky-top" style={{ backgroundColor: '#FF0038' }}>
-            <a className="navbar-brand m-3" href="#"><text className="whiteText bold">Malvan</text></a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse ml-8" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item active">
-                        <a className="nav-link" href="#"><text className="whiteText">Home</text></a>
-                    </li>
-                    <li class="nav-item">
-                        <a className="nav-link" href="#/login"><text className="whiteText">Login</text></a>
-                    </li>
-                    <li class="nav-item">
-                        <a className="nav-link" href="#/mainProd"><text className="whiteText">Pricing</text></a>
-                    </li>
-                </ul>
+        <nav class="navbar navbar-expand-sm bg-light navbar-light fixed-top">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">
+                    <img src="https://tour.pranavkamble.in/mt.jpg" alt=""  height="30" class="d-inline-block align-text-top"/>
+                    MalvaniTourism
+                </a>
+                <button class="navbar-toggler ms-auto ms-sm-2" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle Navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Features</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Pricing</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Dropdown link
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                <li><a class="dropdown-item" href="#">Action</a></li>
+                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </nav>
     )
