1727993844b5f22af14eb61d00832ed47c56dd9e
malvaniTourism <97733182+malvaniTourism@users.noreply.github.com>
2022-07-04 20:41:43 +0530

    Add files via upload

---
+++ src/screens/pages/List.js
@@ -0,0 +1,22 @@
+import React from "react";
+import CustNav from "../../components/navbars/CustNav";
+import Carousel from "../../components/carousel/Carousel";
+import InTouch from "../../components/carousel/InTouch";
+import Newsletter from "../../components/carousel/Newsletter";
+import CustFooter from "../../components/footers/CustFooter";
+import ListFilter from "../../components/carousel/ListFilter";
+
+const ContactUs = () => {
+
+    return (
+        <div>
+            <CustNav />
+            <Carousel />
+            <ListFilter />
+            <Newsletter />
+            <CustFooter />
+        </div>
+    )
+}
+
+export default ContactUs;
