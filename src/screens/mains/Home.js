53e2b7e298962cd9b15d1723d2c6463ed38d9571
Balkrushn3099 <91598511+Balkrushn3099@users.noreply.github.com>
2022-07-08 21:36:55 +0530

    push try

--- src/screens/mains/Home.js
+++ src/screens/mains/Home.js
@@ -1,7 +1,5 @@
 import React from "react";
 import Services from "../../components/banners/Services";
-import IndexHead from "../../components/headers/IndexHead";
-import MainNav from "../../components/navbars/MainNav";
 import HolidaySwiper from "../../components/Swipers/HolidaySwiper";
 import OffrsSwiper from "../../components/Swipers/OffrsSwiper";
 import CafeSwiper from "../../components/Swipers/CafeSwiper";
